0dfd3ecb35b85072d7d84d4e5045116b5c3b0fd4
maria-moroz <mma.moroz.mariia@gmail.com>
2023-09-21 11:55:15 +0100

    Edit tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { technologies } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import TechCard from "./TechCard";
+import SectionWrapper from "./SectionWrapper";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I am good at</p>
+        <h2 className={styles.sectionHeadText}>Technical Skills</h2>
+      </motion.div>
+      <div className='mt-20 justify-center flex flex-wrap xs:gap-10 gap-6'>
+        {technologies.map((technology, index) => (
+          <TechCard key={technology.title} index={index} {...technology} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -7,7 +7,6 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const TechCard = ({ index, title, icon }) => {
   return (
-    // <Tilt className='xs:w-[250px] w-full'>
     <Tilt className='xs:w-[150px] w-[120px]'>
       <motion.div
         variants={fadeIn("right", "spring", 0.4 * index, 0.65)}
@@ -15,7 +14,7 @@ const TechCard = ({ index, title, icon }) => {
       >
         <div
           options={{ max: 45, scale: 1, speed: 450 }}
-          className='bg-tertiary rounded-[20px] py-5 px-8 min-h-[180px] flex justify-evenly items-center flex-col'
+          className='bg-tertiary rounded-[20px] py-5 px-8 min-h-[170px] flex justify-evenly items-center flex-col'
         >
           {icon && (
             <img src={icon} alt={title} className='w-16 h-16 object-contain' />
