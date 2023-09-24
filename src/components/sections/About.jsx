f7ebb2bbd97a7283d2cee050ce24b55e4a2db002
maria-moroz <mma.moroz.mariia@gmail.com>
2023-09-24 08:52:53 +0100

    Change description

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -20,12 +20,9 @@ const About = () => {
         >
           I'm a skilled software developer with experience in TypeScript and
           JavaScript, and expertise in frameworks like React, Node.js and
-          related libraries. My passion for web development has been the driving
-          force behind my journey to create solutions that have the potential to
-          impact millions of users, making their lives easier. <br /> I'm always
-          hungry for knowledge and committed to staying updated with the latest
-          trends, technologies, and best practices to stay at the forefront of
-          web development. <br />
+          related technologies. <br /> I'm always hungry for knowledge and
+          committed to staying updated with the latest trends, technologies, and
+          best practices to stay at the forefront of web development. <br />
           Besides writing clean code and solving complex problems, I combine
           aesthetics with functionality to ensure that the end-users have an
           unforgettable experience.
